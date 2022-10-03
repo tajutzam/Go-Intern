993ae3cdd5b2de281a5bf975f07eb77596939f24
tajutzam <mohammadtajutzamzami07@gmail.com>
2022-10-04 02:06:20 +0700

    fixed mvc template bug

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use LearnPhpMvc\APP\Router;
 use LearnPhpMvc\controller\HomeController;
