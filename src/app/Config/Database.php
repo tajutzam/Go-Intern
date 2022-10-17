a072256cbd31c0e080575301ac6ed6ea8627852c
tajutzam <mohammadtajutzamzami07@gmail.com>
2022-10-17 20:18:28 +0700

    succes connect db

---
+++ src/app/Config/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LearnPhpMvc\Config;
+
+use phpDocumentor\Reflection\Types\Self_;
+
+class Database
+{
+    private static ?\PDO $pdo=null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getConnection(string $env="test") : \PDO{
+        require_once __DIR__."/../../config/dbconfig.php";
+        $config = getDbConfig();
+        if(self::$pdo==null){
+            self::$pdo = new \PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password']
+            );
+        }
+        return self::$pdo;
+    }
+    public function begintTrancsaction(){
+        self::$pdo->beginTransaction();
+    }
+    public function commitTransaction(){
+        self::$pdo->commit();
+    }
+    public function rollback(){
+        self::$pdo->rollBack();
+    }
+
+
+}
---
+++ src/config/dbconfig.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LearnPhpMvc\config;
+
+
+    function getDbConfig():array{
+        return [
+            "database" =>[
+                "test" =>[
+                    "url" => "mysql:host=localhost:3306;dbname=gointern_db_test",
+                    "username"=>"root",
+                    "password"=>""
+                ],
+                "prod"=>[
+                    "url" => "mysql:host=localhost:3306;dbname=gointern_db_test",
+                    "username"=>"root",
+                    "password"=>""
+                ]
+            ]
+        ];
+    }   
