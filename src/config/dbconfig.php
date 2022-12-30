a053d1cf9d70303576b3c00c5a29945ab9d6c842
tajutzam <mohammadtajutzamzami07@gmail.com>
2022-12-30 18:41:42 +0700

    fix link expaired to 15 minute

--- src/config/dbconfig.php
+++ src/config/dbconfig.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace LearnPhpMvc\config;
-
-
     function getDbConfig():array{
         return [
             "database" =>[
                 "test" =>[
-                    "url" => "mysql:host=143.198.198.61;dbname=gointern_db_test",
+                    "url" => "mysql:host=127.0.0.1:3306;dbname=gointern_db_test",
                     "username"=>"root",
                     "password"=>"root"
                 ],
