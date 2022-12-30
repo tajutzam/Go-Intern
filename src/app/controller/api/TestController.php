71f451bcf42ef644faad9c716eb2e07bd0fa6fd1
tajutzam <mohammadtajutzamzami07@gmail.com>
2022-12-30 10:53:19 +0700

    add db pass a

--- src/app/controller/api/TestController.php
+++ src/app/controller/api/TestController.php
@@ -1,7 +1,19 @@
 <?php
 
 namespace LearnPhpMvc\controller\api;
+use LearnPhpMvc\service\PencariMagangService;
+use LearnPhpMvc\service\SekolahService;
+
 class TestController{
+
+
+    private SekolahService $service;
+
+
+    function __construct()
+    {
+        $this->service = new SekolahService();
+    }
 
     function index(){
         echo "adsa";
