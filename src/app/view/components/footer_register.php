396a0f6cd230a0e309fbcfff240f93d3a2f67d3d
tajutzam <mohammadtajutzamzami07@gmail.com>
2023-01-01 11:54:43 +0700

    solve link jti , digital talent

--- src/app/view/components/footer_register.php
+++ src/app/view/components/footer_register.php
@@ -9,15 +9,15 @@ use LearnPhpMvc\config\Url;
     </div>
     <div class="container-fluid text-center">
         <div class="row mt-5">
-            <div class="img1 col-lg-4 mt-5 col-md-12 col-sm-12">
+            <a href="http://jti.polije.ac.id/" class="img1 col-lg-4 mt-5 col-md-12 col-sm-12">
                 <img src=<?= Url::BaseUrl() . "/assets/logo-jti-hitam.png" ?> alt="" style="height: 100px;">
-            </div>
-            <div class="img2 col-lg-4 col-md-12 col-sm-12">
+            </a>
+            <a  href="https://polije.ac.id/" target="_blank" class="img2 col-lg-4 col-md-12 col-sm-12">
                 <img src=<?= Url::BaseUrl() . "/assets/logo-polije.png" ?> alt="" style="height: 200px;">
-            </div>
-            <div class="img3 col-lg-4 col-md-12 col-sm-12" style="margin-top: -60px ;">
+            </a>
+            <a href="https://digitalent.kominfo.go.id/" class="img3 col-lg-4 col-md-12 col-sm-12" style="margin-top: -60px ;">
                 <img src=<?= Url::BaseUrl() . "/assets/logo-kominfo.png" ?> alt="" style="height: 300px;">
-            </div>
+            </a>
         </div>
     </div>
 </div>
